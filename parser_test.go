89d540d9b0a603134d1003521cbaf16461c92316
Feng Tian <fengttt@gmail.com>
2020-06-18 00:39:55 -0700

    parse to json

--- parser_test.go
+++ parser_test.go
@@ -11,4 +11,27 @@ func TestSelect(t *testing.T) {
 		t.Error(err)
 	}
 	t.Log(tree)
+
+	js, err := pg_query.ParseToJSON("select 1")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(js);
+
 }
+
+func TestCreateTable(t *testing.T) {
+	tree, err := pg_query.Parse("create table t (i int, j int) with (url = 'LocateMe!')")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(tree)
+
+	js, err := pg_query.ParseToJSON("create table t (i int, j int) with (url = 'LocateMe!')")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(js);
+}
+
+
